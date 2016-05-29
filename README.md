e97131408f0ca423ce466a94e01ca27c49e1cce8
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2016-05-30 01:14:28 +0300

    Add new ID generator

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 ## React-VideoCall
 Demo app: https://morning-escarpment-67980.herokuapp.com/  
 
+<img align="right" width="360" src="https://raw.githubusercontent.com/nguymin4/react-videocall/master/docs/ss1.jpg"  alt =" " style="border: solid 1px #d4d4d4" />
+  
 Video call to your friend without registering. 
 Simply send your friend your auto-generated unique ID to make the call.  
 
 The app is "Fire-n-Forget" which means that there is no logging at server-side. 
 And everytime you open a new tab, the server give you a totally different unique ID.
+
 
 **Front-end Technologies:**
 - React
@@ -18,4 +21,3 @@ And everytime you open a new tab, the server give you a totally different unique
 - Express
 - Socket.IO
 
-<img align="right" width="360" src="https://github.com/nguymin4/react-videocall/blob/master/docs/ss1.jpg"  alt =" " style="border: solid 1px #d4d4d4" />
--- server/lib/server.js
+++ server/lib/server.js
@@ -1,10 +1,8 @@
-var path = require("path");
-
 var express = require("express"),
 	app = express(),
 	server = require("http").createServer(app),
 	io = require("socket.io"),
-	uuid = require("node-uuid");
+	haiku = require("./haiku");
 
 var userIds = {};
 
@@ -23,23 +21,38 @@ module.exports.run = function (config) {
  */
 function initSocket(socket) {
 	var id;
-	socket.on("init", (data, fn) => {
-		id = uuid.v4().substr(0, 13);
-		userIds[id] = socket;
-		socket.emit("init", { id: id });
-	}).on("call", data => {
-		data["from"] = id;
-		var to = userIds[data.to];
-		if (to) to.emit("call", data);
-		else socket.emit("call", { failed: true });
-	}).on("end", data => {
-		data["from"] = id;
-		var to = userIds[data.to];
-		if (to) to.emit("end");
-	}).on("disconnect", () => {
-		delete userIds[id];
-		console.log(id, "disconnected");
-	});
+	socket
+		.on("init", (data, fn) => {
+			randomID(_id => {
+				id = _id;
+				userIds[id] = socket;
+				socket.emit("init", { id: id });
+			});
+		})
+		.on("call", data => {
+			data["from"] = id;
+			var to = userIds[data.to];
+			if (to) to.emit("call", data);
+			else socket.emit("call", { failed: true });
+		})
+		.on("end", data => {
+			data["from"] = id;
+			var to = userIds[data.to];
+			if (to) to.emit("end");
+		})
+		.on("disconnect", () => {
+			delete userIds[id];
+			console.log(id, "disconnected");
+		});
 
 	return socket;
+}
+
+/**
+ * Random ID until the ID is not in use
+ */
+function randomID(callback) {
+	var id = haiku();
+	if (id in userIds) setTimeout(() => haiku(callback), 5);
+	else callback(id);
 }
