50328ce681dead5b3c2b0b6f7bddc475d83d9e88
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2017-11-05 18:08:29 +0200

    Support react@16 proptypes

--- client/src/js/CallModal.js
+++ client/src/js/CallModal.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'proptypes';
 import classnames from 'classnames';
 
 class CallModal extends Component {
