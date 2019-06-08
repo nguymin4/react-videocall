b6fd308223e298bdcc06d2d178dd42462efce01d
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2019-06-08 13:14:30 +0300

    Move socket logic from server.js to socket.js

--- server/lib/server.js
+++ server/lib/server.js
@@ -1,77 +1,14 @@
 const express = require('express');
 const { createServer } = require('http');
-const io = require('socket.io');
-const haiku = require('./haiku');
+const socket = require('./socket');
 
 const app = express();
 const server = createServer(app);
-const userIds = {};
-const noop = () => {};
 
 app.use('/', express.static(`${process.cwd()}/../client`));
 
-/**
- * Random ID until the ID is not in use
- */
-function randomID(callback) {
-  const id = haiku();
-  if (id in userIds) setTimeout(() => haiku(callback), 5);
-  else callback(id);
-}
-
-/**
- * Send data to friend
- */
-function sendTo(to, done, fail) {
-  const receiver = userIds[to];
-  if (receiver) {
-    const next = typeof done === 'function' ? done : noop;
-    next(receiver);
-  } else {
-    const next = typeof fail === 'function' ? fail : noop;
-    next();
-  }
-}
-
-/**
- * Initialize when a connection is made
- * @param {SocketIO.Socket} socket
- */
-function initSocket(socket) {
-  let id;
-  socket
-    .on('init', () => {
-      randomID((_id) => {
-        id = _id;
-        userIds[id] = socket;
-        socket.emit('init', { id });
-      });
-    })
-    .on('request', (data) => {
-      sendTo(data.to, to => to.emit('request', { from: id }));
-    })
-    .on('call', (data) => {
-      sendTo(
-        data.to,
-        to => to.emit('call', { ...data, from: id }),
-        () => socket.emit('failed')
-      );
-    })
-    .on('end', (data) => {
-      sendTo(data.to, to => to.emit('end'));
-    })
-    .on('disconnect', () => {
-      delete userIds[id];
-      console.log(id, 'disconnected');
-    });
-
-  return socket;
-}
-
 module.exports.run = (config) => {
   server.listen(config.PORT);
+  socket(server);
   console.log(`Server is listening at :${config.PORT}`);
-  io.listen(server, { log: true })
-    .on('connection', initSocket);
 };
-
---
+++ server/lib/socket.js
@@ -0,0 +1,69 @@
+const _ = require('lodash');
+const io = require('socket.io');
+const haiku = require('./haiku');
+
+const userIds = {};
+
+/**
+ * Random ID until the ID is not in use
+ */
+function randomID(callback) {
+  const id = haiku();
+  if (id in userIds) setTimeout(() => haiku(callback), 5);
+  else callback(id);
+}
+
+/**
+ * Send data to friend
+ */
+function sendTo(to, done, fail) {
+  const receiver = userIds[to];
+  if (receiver) {
+    const next = typeof done === 'function' ? done : _.noop;
+    next(receiver);
+  } else {
+    const next = typeof fail === 'function' ? fail : _.noop;
+    next();
+  }
+}
+
+/**
+ * Initialize when a connection is made
+ * @param {SocketIO.Socket} socket
+ */
+function initSocket(socket) {
+  let id;
+  socket
+    .on('init', () => {
+      randomID((_id) => {
+        id = _id;
+        userIds[id] = socket;
+        socket.emit('init', { id });
+      });
+    })
+    .on('request', (data) => {
+      sendTo(data.to, to => to.emit('request', { from: id }));
+    })
+    .on('call', (data) => {
+      sendTo(
+        data.to,
+        to => to.emit('call', { ...data, from: id }),
+        () => socket.emit('failed')
+      );
+    })
+    .on('end', (data) => {
+      sendTo(data.to, to => to.emit('end'));
+    })
+    .on('disconnect', () => {
+      delete userIds[id];
+      console.log(id, 'disconnected');
+    });
+
+  return socket;
+}
+
+module.exports = (server) => {
+  io
+    .listen(server, { log: true })
+    .on('connection', initSocket);
+};
