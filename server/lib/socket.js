23913015f2d48c66d43bb06421eb12b7e8e36267
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2019-06-08 13:41:27 +0300

    Refactor socket.js

--- server/lib/socket.js
+++ server/lib/socket.js
@@ -1,20 +1,5 @@
-const _ = require('lodash');
 const io = require('socket.io');
 const users = require('./users');
-
-/**
- * Send data to friend
- */
-function sendTo(to, done, fail) {
-  const receiver = users.get(to);
-  if (receiver) {
-    const next = typeof done === 'function' ? done : _.noop;
-    next(receiver);
-  } else {
-    const next = typeof fail === 'function' ? fail : _.noop;
-    next();
-  }
-}
 
 /**
  * Initialize when a connection is made
@@ -28,24 +13,29 @@ function initSocket(socket) {
       socket.emit('init', { id });
     })
     .on('request', (data) => {
-      sendTo(data.to, to => to.emit('request', { from: id }));
+      const receiver = users.get(data.to);
+      if (receiver) {
+        receiver.emit('request', { from: id });
+      }
     })
     .on('call', (data) => {
-      sendTo(
-        data.to,
-        to => to.emit('call', { ...data, from: id }),
-        () => socket.emit('failed')
-      );
+      const receiver = users.get(data.to);
+      if (receiver) {
+        receiver.emit('call', { ...data, from: id });
+      } else {
+        socket.emit('failed');
+      }
     })
     .on('end', (data) => {
-      sendTo(data.to, to => to.emit('end'));
+      const receiver = users.get(data.to);
+      if (receiver) {
+        receiver.emit('end');
+      }
     })
     .on('disconnect', () => {
       users.remove(id);
       console.log(id, 'disconnected');
     });
-
-  return socket;
 }
 
 module.exports = (server) => {
