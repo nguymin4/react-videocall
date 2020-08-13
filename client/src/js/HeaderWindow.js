96ed01dcd4be91baef96cee680f38ebd3aa88680
Mike Wolf <mw@mike-wolf.com>
2020-08-13 00:55:06 +0000

    Show tiles only when chatting

--- client/src/js/HeaderWindow.js
+++ client/src/js/HeaderWindow.js
@@ -36,7 +36,7 @@ const HeaderWindow = () => {
 
         // }
 
-    }, [])
+    }, [state.isChatting])
 
 
     const localVideo = React.useRef(null)
@@ -53,17 +53,15 @@ const HeaderWindow = () => {
 
         { (!!state.showCascade) ? null :
             (<React.Fragment>
-                <div className="flex" >
-                    <div className="mt-2 h-25 w-40">
-                        <div className=" h-25 w-40">
-                            <video ref={ localVideo } autoPlay muted />
+                { !state.isChatting ? <div className="mt-2 h-25 w-40">
+                    <div className=" h-25 w-40">
+                        <video ref={ localVideo } autoPlay muted />
 
-                        </div>
-                        <div className=" p-1 h-8 text-black bg-yellow-100">{ state.attrs.name !== 'undefined' ? `${state.attrs.name} (${state.attrs.id})` : state.attrs.id }</div>
                     </div>
-
-                    <VideoTiles />
+                    <div className=" p-1 h-8 text-black bg-yellow-100">{ state.attrs.name !== 'undefined' ? `${state.attrs.name} (${state.attrs.id})` : state.attrs.id }</div>
                 </div>
+
+                    : <VideoTiles /> }
             </React.Fragment>)
         }
     </div>
--- client/src/js/VideoTiles.js
+++ client/src/js/VideoTiles.js
@@ -35,24 +35,27 @@ const VideoTiles = () => {
 
     }, [refs, state.users, state.isChatting])
     return <React.Fragment>
-        { state.allSessions.map((key, index) => {
-            const user = json(state.users[key])
-            if (!user) return null
-            return <div key={ key } className="m-2 h-25 w-40" >
-                <div className=" h-24 text-black bg-gray-800  ">
-                    <video ref={ el => {
-                        if (el) {
-                            console.log("set refs ", key, index)
-                            refs[key] = el
-                            setRefs(refs)
-                        }
-                    } } autoPlay muted />
+        <div className="flex" >
 
+            { state.allSessions.map((key, index) => {
+                const user = json(state.users[key])
+                if (!user) return null
+                return <div key={ key } className="m-2 h-25 w-1/4" >
+                    <div className=" text-black bg-gray-800  ">
+                        <video ref={ el => {
+                            if (el) {
+                                console.log("set refs ", key, index)
+                                refs[key] = el
+                                setRefs(refs)
+                            }
+                        } } autoPlay muted />
+
+                    </div>
+                    <div className="p-1 h-8 text-black bg-yellow-100" >
+                        { user.name } ({ user.control })</div>
                 </div>
-                <div className="p-1 h-8 text-black bg-yellow-100" >
-                    { user.name } ({ user.control })</div>
-            </div>
-        }) }
+            }) }
+        </div>
 
     </React.Fragment>
 }
