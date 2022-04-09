f85343047db12df9658e1e801a1b4fdf9d757d7a
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2022-04-10 01:40:52 +0300

    refactor: use createRoot react

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './js/App';
 import './css/app.scss';
 
-render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
