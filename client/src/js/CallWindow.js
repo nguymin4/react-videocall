43a2082f86f9466f5d60644a414306f01c0b5b1b
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2022-04-10 00:24:25 +0300

    fix: linting issue

--- client/src/js/CallWindow.js
+++ client/src/js/CallWindow.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
